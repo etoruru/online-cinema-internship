b562745ea4acc7325b1a0b89b19238edf34b2b28
Nadezhda <nadezhda.shatalina0@gmail.com>
2022-10-26 15:32:58 +0300

    try to make path

--- online_cinema/encoder/models/video.py
+++ online_cinema/encoder/models/video.py
@@ -1,8 +1,19 @@
+import os
+import uuid
+
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from config.settings import base
+
+
+def _make_directories():
+    uid = str(uuid.uuid4())
+    first_dir, _ = uid.split("-", maxsplit=1)
+    filepath = os.path.join(base.MEDIA_DIR, "videofiles")
+    os.makedirs(filepath)
+    return filepath
 
 
 class Video(models.Model):
@@ -14,6 +25,7 @@ class Video(models.Model):
 
     filename = models.CharField(max_length=200)
     filepath = models.CharField(max_length=200)
+    # videofile = models.FileField(upload_to=_make_directories)
     created_by = models.ForeignKey(
         base.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="videos"
     )
