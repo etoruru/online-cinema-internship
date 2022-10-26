200af6c3e12c4b9d5ba0834a7fbbbadc2d075440
Nadezhda <nadezhda.shatalina0@gmail.com>
2022-10-26 15:36:56 +0300

    deleted unused things

--- online_cinema/encoder/tasks.py
+++ online_cinema/encoder/tasks.py
@@ -1,20 +1,14 @@
 import os
 import subprocess
-import uuid
-
-from celery import shared_task
 
 from config import celery_app
 from config.settings import base
 from online_cinema.encoder.convert import convert_to_mp4
-from online_cinema.encoder.models import Video
 
 
 @celery_app.task()
 def convert_video(video_path_out):
     width = 720
-    # video = Video.objects.get(pk=video_id)
-    # video_path_out = video.filepath
     filename = "video.mov"
     video_path_input = os.path.join(base.ROOT_DIR, "online_cinema/videofiles/")
     name, _ = os.path.splitext(filename)
