d0d5e6793e0fdbd82b4099393df4fb8f57f6188f
Nadezhda <nadezhda.shatalina0@gmail.com>
2022-11-15 14:50:22 +0300

    created access for film from episode

--- online_cinema/cards/api_router.py
+++ online_cinema/cards/api_router.py
@@ -1,6 +1,6 @@
 from rest_framework_extensions.routers import ExtendedDefaultRouter
 
-from online_cinema.comments.views import CommentViewSet
+from online_cinema.encoder.views import ConvertTaskViewSet
 
 from . import views
 
@@ -12,9 +12,6 @@ router.register(r"cards", views.CardViewSet, basename="cards").register(
     parents_query_lookups=["card__episode", "season"],
 )
 router.register(r"seasons", views.SeasonViewSet)
-router.register(r"episodes", views.EpisodeViewSet).register(
-    r"comments", CommentViewSet, "cards-comment", parents_query_lookups=["episode"]
-)
 router.register(r"genres", views.GenreViewSet).register(
     "cards", views.CardViewSet, "genre_cards", parents_query_lookups=["genres"]
 )
@@ -22,3 +19,6 @@ router.register(r"countries", views.CountryViewSet).register(
     "cards", views.CardViewSet, "countries_cards", parents_query_lookups=["country"]
 )
 router.register(r"membership", views.MembershipViewSet)
+router.register(r"episodes", views.EpisodeViewSet).register(
+    r"videos", ConvertTaskViewSet, "cards-video", parents_query_lookups=["video"]
+)
